68a9e1f3a3e834ee5590dce8387d981327a57f1e
Rmunyua <61853106+Rmunyua@users.noreply.github.com>
2020-04-05 23:08:44 -0400

    Update poll_main.py

--- 03Python/python-challenge/PyPoll/poll_main.py
+++ 03Python/python-challenge/PyPoll/poll_main.py
@@ -5,32 +5,25 @@ outputfile = os.path.join("election_results")
 
 #Declare all variables
 total_votes = 0
-num_votes = 0
-winner = str ()
-name = ["","",""]
-k_vote = 0
-c_vote = 0
-l_vote = 0
-o_vote = 0
-k_per = 0
-c_per = 0
-l_per = 0
-o_per = 0
+candidate_name = 0
+candidate_votes = {}
+winner = ""
+winner_count = 0
+
 
 #Read and open the cvs file
 #Calculate number of votes
 with open(csvpoll) as csvfile:
     reader = csv.reader(csvfile)  
+    header = next(reader)
 
     for row in (reader):
         total_votes = total_votes + 1
-#Print list of candidates
-        # if row[2] == "Khan":
-        #     name = row[2]
-        # if row[2] != "Khan":
-        #     name =  row[2]
-        #     else 
-        #         pass
+
+# And begin tracking that candidate’s voter count
+       total_votes[candidate_name] = 0
+       total_votes[candidate_name] = total_votes[candidate_name] + 1
+        if 
 #Show the number of votes for each candidate
         if row[2] == "Khan":
             k_vote = k_vote + 1 
@@ -42,14 +35,24 @@ with open(csvpoll) as csvfile:
             o_vote = o_vote + 1
 
 #Show the percentage of votes for each candidate 
-        k_per = (k_vote + c_vote + l_vote + o_vote) / .100
-        c_per = (k_vote + c_vote + l_vote + o_vote) / .100
-        l_per = (k_vote + c_vote + l_vote + o_vote) / .100
-        o_per = (k_vote + c_vote + l_vote + o_vote) / .100
-#Show the winner of the election by popular vote 
+        
+        vote_percentage = float(k_votes) / float(total_votes) * 100
+        vote_percentage = float(c_votes) / float(total_votes) * 100
+        vote_percentage = float(l_votes) / float(total_votes) * 100
+        vote_percentage = float(o_votes) / float(total_votes) * 100
 
+        # k_per = (k_vote + c_vote + l_vote + o_vote) / .100
+        # c_per = (k_vote + c_vote + l_vote + o_vote) / .100
+        # l_per = (k_vote + c_vote + l_vote + o_vote) / .100
+        # o_per = (k_vote + c_vote + l_vote + o_vote) / .100
+#Show the winner of the election by popular vote 
+    print(". ", end="")
 #open & write the text file for the Election Results
 with open(outputfile, "w") as file:
+    output = f”{candidate}: {vote_percentage:.3f}% ({votes})\n”
+        print(voter_output, end=“”)
+
+
     file.write("election_results")
     file.write("------------------")
     file.write("Khan: "+ str(k_per) + "%" + str())
@@ -58,6 +61,7 @@ with open(outputfile, "w") as file:
     file.write("O'Tooley: " + str(o_per) + "%" + str())
     file.write("------------------")
     file.write("Winner: " + str())
+
 #Print Totals
 print(total_votes)
 print(k_vote)
